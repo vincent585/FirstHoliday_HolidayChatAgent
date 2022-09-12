8be492c7aaaad1cb4ded34854267172acbeaf589
Vincent Klimjack <Vincent.Klimjack@jet2.com>
2022-09-12 15:21:13 +0100

    implement DbConnectionFactory, HolidayRepository, Add Holiday DTO, add Default entry for connection string

---
+++ HolidayChatAgent/HolidayChatAgent.Repository/DTOs/HolidayDto.cs
@@ -0,0 +1,15 @@
+﻿namespace HolidayChatAgent.Repository.DTOs
+{
+    public class HolidayDto
+    {
+        public int Id { get; set; }
+        public string City { get; set; } = default!;
+        public string Continent { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public string Category { get; set; } = default!;
+        public int StarRating { get; set; }
+        public string TempRating { get; set; } = default!;
+        public string Location { get; set; } = default!;
+        public decimal PricePerNight { get; set; }
+    }
+}
---
+++ HolidayChatAgent/HolidayChatAgent.Repository/DbConnection/IDbConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace HolidayChatAgent.Repository.DbConnection
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection Create();
+    }
+}
