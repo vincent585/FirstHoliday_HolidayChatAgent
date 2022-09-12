1bf913acbc06e1b7e215b9ae68242bdc7aff3422
Vincent Klimjack <Vincent.Klimjack@jet2.com>
2022-09-12 14:09:11 +0100

    add database project and tables

---
+++ HolidayChatAgent/HolidayChatAgent.Database/Tables/Holidays.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Holidays]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [City] NVARCHAR(100) NOT NULL, 
+    [ContinentId] INT NOT NULL, 
+    [CountryId] INT NOT NULL, 
+    [CategoryId] INT NOT NULL, 
+    [StarRating] INT NULL, 
+    [TempRatingId] INT NOT NULL, 
+    [LocationId] INT NOT NULL, 
+    [PricePerNight] DECIMAL(9, 2) NOT NULL, 
+    CONSTRAINT [FK_Holidays_Continents_ContinentId] FOREIGN KEY ([ContinentId]) REFERENCES [Continents]([Id]),
+    CONSTRAINT [FK_Holidays_Countries_CountryId] FOREIGN KEY ([CountryId]) REFERENCES [Countries]([Id]),
+    CONSTRAINT [FK_Holidays_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Categories]([Id]),
+    CONSTRAINT [FK_Holidays_TempRatings_TempRatingId] FOREIGN KEY ([TempRatingId]) REFERENCES [TempRatings]([Id]),
+    CONSTRAINT [FK_Holidays_Locations_LocationId] FOREIGN KEY ([LocationId]) REFERENCES [Locations]([Id])
+)
---
+++ HolidayChatAgent/HolidayChatAgent.Database/Tables/Locations.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Locations]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Description] NVARCHAR(50) NOT NULL
+)
