a67e2c8b00ba2422a6ba7db29e7748815fb6594d
Vincent Klimjack <Vincent.Klimjack@jet2.com>
2022-09-12 10:40:24 +0100

    add MVC template

---
+++ HolidayChatAgent/HolidayChatAgent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HolidayChatAgent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
