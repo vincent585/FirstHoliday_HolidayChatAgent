7e86d40e1de80dafb8f66a61d5c4deb003112343
Vincent Klimjack <Vincent.Klimjack@jet2.com>
2022-09-12 17:12:46 +0100

    added skeleton classes for service layer, added AutoMapper and domain Model

---
+++ HolidayChatAgent/HolidayChatAgent.Services/Interfaces/IHolidayService.cs
@@ -0,0 +1,9 @@
+﻿using HolidayChatAgent.Services.Models.Domain;
+
+namespace HolidayChatAgent.Services.Interfaces
+{
+    public interface IHolidayService
+    {
+        Task<IEnumerable<HolidayDetail>> GetRecommendedHolidaysAsync();
+    }
+}
---
+++ HolidayChatAgent/HolidayChatAgent.Services/Services/HolidayService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HolidayChatAgent.Repository.Interfaces;
+using HolidayChatAgent.Services.Interfaces;
+using HolidayChatAgent.Services.Models.Domain;
+
+namespace HolidayChatAgent.Services.Services
+{
+    public class HolidayService : IHolidayService
+    {
+        private readonly IMapper _mapper;
+        private readonly IHolidayRepository _holidayRepository;
+
+        public HolidayService(IMapper mapper, IHolidayRepository holidayRepository)
+        {
+            _mapper = mapper;
+            _holidayRepository = holidayRepository;
+        }
+
+        public async Task<IEnumerable<HolidayDetail>> GetRecommendedHolidaysAsync()
+        {
+            var holidayDtos = await _holidayRepository.GetAllHolidaysAsync();
+
+            // TODO - Create and implement a IHolidayFilter to call _holidayFilter.FilterHolidays(holidayDtos);
+
+            throw new NotImplementedException();
+        }
+    }
+}
