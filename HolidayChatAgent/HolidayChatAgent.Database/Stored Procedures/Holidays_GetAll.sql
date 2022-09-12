9fd7a6d4afaee88094a34b52041fbd815224b25a
Vincent Klimjack <Vincent.Klimjack@jet2.com>
2022-09-12 14:47:26 +0100

    add GetAll stored proc

---
+++ HolidayChatAgent/HolidayChatAgent.Database/Stored Procedures/Holidays_GetAll.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[Holidays_GetAll]
+AS
+
+SELECT
+	City,
+	CONT.Name AS Continent,
+	CTRY.Name AS Country,
+	CAT.Description AS Category,
+	StarRating,
+	TR.Description AS TempRating,
+	L.Description AS Location,
+	PricePerNight
+
+	
+FROM
+	[dbo].[Holidays] H
+	INNER JOIN [dbo].[Continents] CONT ON CONT.Id = H.ContinentId
+	INNER JOIN [dbo].[Countries] CTRY ON CTRY.Id = H.CountryId
+	INNER JOIN [dbo].[Categories] CAT ON CAT.Id = H.CategoryId
+	INNER JOIN [dbo].[TempRatings] TR ON TR.Id = H.TempRatingId
+	INNER JOIN [dbo].[Locations] L ON L.Id = H.LocationId
